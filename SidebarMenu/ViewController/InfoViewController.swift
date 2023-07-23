c83737c8c6829e96c72cf2406ad0072b573c40f0
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-07-24 01:36:37 +0300

    add_project

---
+++ SidebarMenu/ViewController/InfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoViewController.swift
+//  SidebarMenu
+//
+//  Created by Николай Гринько on 24.07.2023.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Info privet"
+        view.backgroundColor = .systemGreen
+    }
+    
+}
